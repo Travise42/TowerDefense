7f519f6292c123d4b1ed876e1a95f32115279f53
Travise <travise42@icloud.com>
2023-12-09 15:52:09 -0500

    Added enemy animation

--- app/src/main/java/towerdefense/game/enemies/Enemy.java
+++ app/src/main/java/towerdefense/game/enemies/Enemy.java
@@ -20,12 +20,14 @@ public class Enemy {
     private Game game;
 
     private float type;
-    private int x;
-    private int y;
+    private float x;
+    private float y;
     private int size;
-    private int speed;
+    private float speed;
     private int damage;
     private int health;
+
+    private int animationFrame;
 
     public Enemy( Game gameInstance, float enemy_type ) {
         game = gameInstance;
@@ -38,14 +40,24 @@ public class Enemy {
         float tileSize = gameInstance.map.getTileSize();
 
         size = (int) ( type * tileSize ); // 20% - 180% of a square
-        speed = (int) ( ( 2 - enemy_type ) * tileSize ); // 20% - 180% of a square per frame
+        speed = ( 2 - enemy_type ) * tileSize; // 20% - 180% of a square per frame
+
+        animationFrame = 0;
     }
 
     public void draw( Graphics g ) {
-        int screenX = ( int )( x - game.camera.getX() );
-        int screenY = ( int )( y - game.camera.getY() );
+        int ex = ( int )( x - game.camera.getX() );
+        int ey = ( int )( y - game.camera.getY() );
         g.setColor( Color.RED );
-        drawCircle( g, screenX, screenY, size );
+        drawCircle( g, ex, ey, size );
+
+        ////double a = Math.PI/8 * ( Math.cos( animationFrame / 1000f ) + 2 );
+        ////drawCircle( g, ex + ( int )( size*Math.cos( a ) / 2 ), ey + ( int )( size*Math.sin( a ) / 2 ), size/3 );
+        ////drawCircle( g, ex + ( int )( size*Math.cos( a - Math.PI/2f ) / 2 ), ey - ( int )( size*Math.sin( a + Math.PI/2f ) / 2 ), size/3 );
+    
+        double a = Math.sin( animationFrame / 600f );
+        drawCircle( g, ( int )( ex - size*( a - 1 ) / 4 ), ( int )( ey + size*( a + 5 ) / 12 ), size / 3 );
+        drawCircle( g, ( int )( ex + size*( a + 1 ) / 4 ), ( int )( ey + size*( a - 5 ) / 12 ), size / 3 );
     }
 
     private void drawCircle( Graphics g, int x, int y, int diameter ) {
@@ -53,7 +65,16 @@ public class Enemy {
     }
 
     public void move() {
-        x += 1;
+        x += speed / 100f;
+        animationFrame += speed;
+    }
+    
+    public int getX() {
+        return (int) x;
+    }
+    
+    public int getY() {
+        return (int) y;
     }
     
 }

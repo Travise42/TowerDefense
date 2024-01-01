ce78a2c73c9b2dfefc896ca23724204d2ba45d31
Travise <travise42@icloud.com>
2023-12-31 19:31:46 -0500

    Optimized Pathfinding

--- app/src/main/java/towerdefense/game/enemies/EnemyMovement.java
+++ app/src/main/java/towerdefense/game/enemies/EnemyMovement.java
@@ -1,7 +1,5 @@
 package towerdefense.game.enemies;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,7 +15,7 @@ public class EnemyMovement {
 
     private List<Integer> brain;
     public int[][] path;
-    
+
     public EnemyMovement(boolean mode) {
         this.mode = mode;
 
@@ -25,65 +23,57 @@ public class EnemyMovement {
     }
 
     public void generatePath() {
-        path = new int[Game.instance.map.map.getOpenColumns()][Game.instance.map.map.getOpenRows()];
+        final int OPEN_COLUMNS = Game.instance.map.map.getOpenColumns();
+        final int OPEN_ROWS = Game.instance.map.map.getOpenRows();
+
+        final int LEFT_COLUMN = (Map.COLUMNS - OPEN_COLUMNS) / 2;
+        final int TOP_ROW = (Map.ROWS - OPEN_ROWS) / 2;
+
+        // Empty path data
+        path = new int[OPEN_COLUMNS][OPEN_ROWS];
 
         LinkedList<int[]> POIs = new LinkedList<>();
-        POIs.add( new int[] { path.length, path[0].length / 2 - 1 } );
-        POIs.add( new int[] { path.length, path[0].length / 2 } );
-
         LinkedList<int[]> delayedPOIs = new LinkedList<>();
 
-        final int LEFT_COLUMN = ( Map.COLUMNS - Game.instance.map.map.getOpenColumns() ) / 2;
-        final int TOP_ROW = ( Map.ROWS - Game.instance.map.map.getOpenRows() ) / 2;
+        // Start at entrance
+        POIs.add(new int[] { OPEN_COLUMNS, OPEN_ROWS / 2 - 1 });
+        POIs.add(new int[] { OPEN_COLUMNS, OPEN_ROWS / 2 });
 
-        while ( POIs.size() + delayedPOIs.size() > 0 ) {
+        while (!(POIs.isEmpty() && delayedPOIs.isEmpty())) {
             int size = POIs.size();
 
-            for ( int i = 0; i < size; i++ ) {
-                int[] poi = POIs.get( 0 );
-                POIs.remove( 0 );
+            // Iterate through all points of interest that were created last iteration
+            for (; 0 < size--;) {
+                // Pull out the first point of interest
+                int[] poi = POIs.poll();
 
-                for ( int m = -1; m <= 1; m += 2 ) {
-                    for ( int x = 1; x > -1; x-- ) {
-                        int c = poi[0] + x * m;
-                        int r = poi[1] + ( 1 - x ) * m;
+                // Check all bounding squares
+                for (int m = -1; m <= 1; m += 2) {
+                    for (int x = 1; x >= 0; x--) {
+                        int column = poi[0] + x * m;
+                        int row = poi[1] + (1 - x) * m;
 
-                        if ( Game.instance.map.map.getOpenColumns() <= c || c < 0 || Game.instance.map.map.getOpenRows() <= r || r < 0
-                            || path[c][r] != 0 ) continue;
-
-                        path[c][r] = m * ( x + 1 );
-
-                        if ( !Game.instance.map.map.isOpen( c + LEFT_COLUMN, r + TOP_ROW ) ) {
-                            delayedPOIs.add( new int[] { c, r, 5 } );
+                        boolean OUT_OF_BOUNDS = column < 0 || column >= OPEN_COLUMNS || row < 0 || row >= OPEN_ROWS;
+                        if (OUT_OF_BOUNDS)
                             continue;
-                        }
 
-                        POIs.add( new int[] { c, r } );
+                        boolean DUPLICATE = path[column][row] != 0;
+                        if (DUPLICATE)
+                            continue;
+
+                        path[column][row] = m * (x + 1);
+
+                        // Add the new point of interest if area open, otherwise, save it for later
+                        if (Game.instance.map.map.isOpen(column + LEFT_COLUMN, row + TOP_ROW)
+                                || !delayedPOIs.add(new int[] { column, row, 5 })) {
+                            POIs.add(new int[] { column, row });
+                        }
                     }
                 }
             }
-
-            for ( int d = 0; d < delayedPOIs.size(); ) {
-                if ( --delayedPOIs.get( d )[2] > 0 ) {
-                    d++;
-                    continue;
-                }
-
-                POIs.add( delayedPOIs.get( d ) );
-                delayedPOIs.remove( d );
-            }
+            // Move delayed points of interest to POIs if their delay is over
+            delayedPOIs.removeIf(poi -> --poi[2] == 0 && POIs.add(poi));
         }
-
-        // for ( int row = 0; row < path[0].length; row++ ) {
-        //     for ( int[] column : path ) {
-        //         System.out.print( column[ row ] + ",\t" );
-        //     }
-        //     System.out.println();
-        // }
-        // System.out.println();
-        //          1: up
-        // 2: left        -2: right
-        //        -1: down
     }
 
 }
